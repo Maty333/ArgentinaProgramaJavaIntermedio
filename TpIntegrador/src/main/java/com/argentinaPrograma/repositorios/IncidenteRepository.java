9d096e505e0c3f61832d1546f2af54f76220cced
Maty333 <126540155+Maty333@users.noreply.github.com>
2023-11-26 18:47:58 -0300

    Delete TpIntegrador directory

--- TpIntegrador/src/main/java/com/argentinaPrograma/repositorios/IncidenteRepository.java
+++
@@ -1,8 +0,0 @@
-package com.argentinaPrograma.repositorios;
-
-import com.argentinaPrograma.model.IncidenteEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface IncidenteRepository extends JpaRepository<IncidenteEntity, Long> {
-   
-}
--- TpIntegrador/src/main/java/com/argentinaPrograma/servicios/TecnicoServicio.java
+++
@@ -1,5 +0,0 @@
-package com.argentinaPrograma.servicios;
-
-public class TecnicoServicio {
-
-}
